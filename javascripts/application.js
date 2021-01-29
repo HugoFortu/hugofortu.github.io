b1fe9783f86c1f960e0c0cb655efca678b8985a5
hugo fortunier <hfortunier@gmail.com>
2021-01-29 15:10:27 +0100

    Automated commit at 2021-01-29 14:10:27 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,1 +1,5 @@
 // This is where it all goes :)
+var contact = document.querySelector('#contact')
+contact.addEventListener('click', ()=> {
+    document.getElementById('check').checked = true;
+});
