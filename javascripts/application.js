f58616cb31ac64d209ccfd4e25f2426021d1580a
hugo fortunier <hfortunier@gmail.com>
2021-02-12 12:19:13 +0100

    Automated commit at 2021-02-12 11:19:13 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/application.js
+++ javascripts/application.js
@@ -7,10 +7,16 @@ if (window.matchMedia("(max-width: 1000px)").matches) {
   })
 }
 
-const source = document.querySelector('.source');
+if (window.matchMedia("(min-width: 1001px)").matches) {
+  var contact = document.querySelector('.contact')
+  contact.addEventListener('click', ()=> {
+      window.scroll(0,1000000);
+  })
+}
+// const source = document.querySelector('.source');
 
-source.addEventListener('copy', (event) => {
-    const selection = document.getSelection();
-    event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
-    event.preventDefault();
-});
+// source.addEventListener('copy', (event) => {
+//     const selection = document.getSelection();
+//     event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
+//     event.preventDefault();
+// });
